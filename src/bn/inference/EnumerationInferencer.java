9564ba5624f585b7d0ac4e434b5cc2941e0ae8b7
Steven <jgu8@u.rochester.edu>
2019-03-24 22:41:22 -0400

    Enumeration Inferencer Update

--- src/bn/inference/EnumerationInferencer.java
+++ src/bn/inference/EnumerationInferencer.java
@@ -17,42 +17,43 @@ public class EnumerationInferencer extends java.lang.Object implements Inference
 
     @Override
     public Distribution query(RandomVariable X, Assignment e, BayesianNetwork network) {
-        return new Distribution(X);
+		Distribution Q = new bn.base.Distribution(X);
+		bn.core.Domain xDomain = X.getDomain();
+		List<RandomVariable> vars = network.getVariablesSortedTopologically();
+		for(Value xi:xDomain) {
+			Assignment cpy=e.copy();
+			cpy.put(X, xi);
+			double r=enumerateAll(network,cpy,network.getVariablesSortedTopologically());
+			Q.set(xi, r);
+		} 
+		Q.normalize();
+	return Q; 	
     }
-    
-    public Distribution enumerationASK(RandomVariable X, Assignment e, BayesianNetwork network) {
-    		CPT Q = new CPT(X);
-    		bn.core.Domain xDomain = X.getDomain();
-    		List<RandomVariable> vars = network.getVariablesSortedTopologically();
-    		for(Value s:xDomain) {
-    			Q.set(s, e, enumerateAll(network,e,vars));
-			//where ey is extended with Y=y
-    		}
-    		Distribution result = new Distribution(Q.getVariable());
-    		result.normalize();
-		return result; 	
-    }
-    
+
     public double enumerateAll(BayesianNetwork network, Assignment e,List<RandomVariable> vars) {
     		//List<RandomVariable> vars = network.getVariablesSortedTopologically();
     		if(vars.isEmpty()) {
     			return 1.0;
     		}
-    		RandomVariable Y = vars.get(0);
+			RandomVariable Y = vars.get(0);
+			/*
     		Set<RandomVariable> parent = network.getParents(Y);
 			Assignment parentA = new bn.base.Assignment();
 			for(RandomVariable r:parent) {
 				parentA.put(r, BooleanValue.TRUE);
 			}
 			vars.remove(0);
+			*/
     		if(e.containsValue(Y)) {
-    			return network.getProbability(Y, parentA)*enumerateAll(network,e,vars);
+				Assignment cpy=e.copy();
+				return network.getProbability(Y, cpy)*enumerateAll(network, cpy, vars.subList(1, vars.size()));
     		}else {
     			double result = 0;
     			bn.core.Domain yDomain = Y.getDomain();
     			for(Value y:yDomain) {
-    				//where ey is extended with Y=y
-    				result+=network.getProbability(Y, parentA)*enumerateAll(network,e,vars);
+					Assignment cpy=e.copy();
+					cpy.put(Y,y);
+					result += network.getProbability(Y, cpy)*enumerateAll(network, cpy, vars.subList(1, vars.size()));
     			}
     			return result;
     		}
