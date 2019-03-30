5697278ca1ad122c3a55b46b0d6f983793ebdcd9
Shuran Zhang <szhang73@u.rochester.edu>
2019-03-29 21:53:43 -0400

    update

--- src/bn/inference/RejectionSamplingInferencer.java
+++ src/bn/inference/RejectionSamplingInferencer.java
@@ -60,7 +60,17 @@ public class RejectionSamplingInferencer extends java.lang.Object {
 
 		return result;
 	}
-	
+	public double largestProb(RandomVariable r,BayesianNetwork bn,Assignment e) {
+		PriorityQueue<Double> pQueue = new PriorityQueue<Double>(); 
+		int dSize = r.getDomain().size();
+		ArrayList<Double> prob = new ArrayList<Double>();
+		Node n = bn.getNodeForVariable(r);
+		for(int i=0;i<dSize;i++) {
+			double p=n.cpt.get(getValue(i,r.getDomain()),e);
+			pQueue.add(p);
+		}
+		return (1-pQueue.peek());
+	}
 	public Assignment pSample(BayesianNetwork network) {
 		Assignment event = new bn.base.Assignment();
 		for(RandomVariable r: network.getVariablesSortedTopologically()) {
@@ -75,9 +85,12 @@ public class RejectionSamplingInferencer extends java.lang.Object {
 	
 
 	public Value randomSample(BayesianNetwork network,Assignment e,RandomVariable r) {
+		double d = largestProb(r, network, e);
 		int dSize=  r.getDomain().size();
 		double counter = 0;
-		double random = Math.random();
+		Random rd = new Random();
+		double random = 0 + (d - 0) * rd.nextDouble();
+		//System.out.println(random);
 		double p = 0;
 		for(int i=0;i<dSize;i++) {
 			Assignment temp = e.copy();
