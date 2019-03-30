1d7798cfbbd3b911cfa3c993426284565d92b61e
yoliziyizy <44129677+yoliziyizy@users.noreply.github.com>
2019-03-29 22:27:23 -0400

    Update WeightedSampler.java

--- src/bn/inference/WeightedSampler.java
+++ src/bn/inference/WeightedSampler.java
@@ -1,5 +1,15 @@
 package bn.inference;
 
+import bn.core.Assignment;
+
 public class WeightedSampler extends PriorSampler{
-    
+	
+	        Assignment assignment;
+	        double weight = 0.0;
+
+	        WeightedSampler(Assignment a, double weight){
+	            this.assignment = a;
+	            this.weight = weight;
+	        }
+	     
 }
