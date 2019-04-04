32d0b803678241f95432bf1e251056cc40f2c7a8
Shuran Zhang <szhang73@u.rochester.edu>
2019-04-04 14:18:47 -0400

    Add files via upload

--- src/bn/inference/MarkovBlanketSampler.java
+++ src/bn/inference/MarkovBlanketSampler.java
@@ -1,5 +1,18 @@
 package bn.inference;
 
-public class MarkovBlanketSampler extends java.lang.Object{
-    
+import bn.core.Assignment;
+import bn.core.RandomVariable;
+import bn.core.Value;
+
+public class MarkovBlanketSampler extends PriorSampler{
+	
+	        Assignment assignment;
+	        double counter = 0.0;
+
+	        MarkovBlanketSampler(Assignment a){
+	            this.assignment = a;
+	        }
+
+	
+	     
 }
